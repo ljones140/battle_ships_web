bd2be7a2f8ed659a1fa5003eafd6525aa9abc9ff
Toby Clarke <teeohbee@gmail.com>
2015-08-04 11:38:47 +0100

    created index and newgame page

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+  set :views, Proc.new { File.join(root, "..", "views") }
   get '/' do
     'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/newgame' do
+    "New Game"
+    erb :newgame
   end
 
   # start the server if ruby file executed directly
